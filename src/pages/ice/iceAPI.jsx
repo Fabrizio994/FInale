b078ca32ddd10a54fe5f393f4d1bb0ccd1c6d2c8
Fabrizio Recchia <fabrizio643@hotmail.it>
2024-09-15 08:31:42 +0200

    fix api

--- src/pages/ice/iceAPI.jsx
+++ src/pages/ice/iceAPI.jsx
@@ -1,25 +1,26 @@
 import { useQuery } from "react-query";
 
-export function useIceData() {
-  return useQuery("iceData", async () => {
-    const response = await fetch("https://global-warming.org/api/arctic-api");
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const result = await response.json();
+const fetchIceData = async () => {
+  const response = await fetch("https://global-warming.org/api/arctic-api");
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  const result = await response.json();
 
-    const filteredData = Object.entries(result.arcticData.data)
-      .filter(([key]) => key !== "198712" && key !== "198801")
-      .map(([key, value]) => {
-        const year = key.slice(0, 4);
-        const month = key.slice(4, 6);
-        return {
-          yearMonth: `${year},${month}`,
-          value: value.value,
-        };
-      });
+  const filteredData = Object.entries(result.arcticData.data)
+    .filter(([key]) => key !== "198712" && key !== "198801")
+    .map(([key, value]) => {
+      const year = key.slice(0, 4);
+      const month = key.slice(4, 6);
+      return {
+        yearMonth: `${year},${month}`,
+        value: value.value,
+      };
+    });
 
-    console.log(result);
-    return filteredData;
-  });
-}
+  return filteredData;
+};
+
+export const useIceData = () => {
+  return useQuery("iceData", fetchIceData);
+};
