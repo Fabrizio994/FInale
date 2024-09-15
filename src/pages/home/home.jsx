f369a6488053c462ea33acc1d35aadc25987a7dc
Fabrizio Recchia <fabrizio643@hotmail.it>
2024-09-15 06:25:30 +0200

    Fix h1

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,7 +6,9 @@ function Home() {
   return (
     <>
       <Navbar />
+
       <div className="homePage">
+        <h1 className="mt-5 display-1 ">GlobalStats</h1>
         <div className="homeCont">
           <p className=" homeP">{homeData.description}</p>
         </div>
